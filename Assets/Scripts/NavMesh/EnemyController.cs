91c76095913ff683e9efab25403cb090904c6b8e
Osman Orhan Askin <115871580+osmanAskin@users.noreply.github.com>
2026-01-31 21:15:13 +0300

    F

--- Assets/Scripts/NavMesh/EnemyController.cs
+++ Assets/Scripts/NavMesh/EnemyController.cs
@@ -1,45 +1,61 @@
 using UnityEngine;
 using UnityEngine.AI;
-using System.Collections;
 
 public class EnemyController : MonoBehaviour 
 {
- public float lookRadius = 10f;
- Transform target;
- [SerializeField] private NavMeshAgent agent;
+    public float lookRadius = 10f;
+    Transform target;
+    [SerializeField] private NavMeshAgent agent;
+    private Animator animator;
 
- private void Start()
- {
-  target = PlayerManager.instance.player.transform;
-  agent = GetComponent<NavMeshAgent>();
- }
+    private void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
  
- private void Update()
- {
-  float distance = Vector3.Distance(target.position, transform.position);
+    private void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
 
-  if (distance <= lookRadius)
-  {
-   agent.SetDestination(target.position);
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
 
-   if (distance <= agent.stoppingDistance)
-   {
-    //here attack
-    FaceTarget();
-   }
-  }
- }
+            if (distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+                HandleAnimations(false, true);
+            }
+            else 
+            {
+                HandleAnimations(true, false);
+            }
+        }
+        else 
+        {
+            HandleAnimations(false, false);
+        }
+    }
+
+    void HandleAnimations(bool isWalking, bool isAttacking)
+    {
+        animator.SetBool("IsWalking", isWalking);
+        animator.SetBool("IsAttack", isAttacking);
+        animator.SetBool("IsIdle", !isWalking && !isAttacking);
+    }
  
- void FaceTarget()
- {
-  Vector3 direction = (target.position - transform.position).normalized;
-  Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-  transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
- }
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
  
- private void OnDrawGizmosSelected()
- {
-  Gizmos.color = Color.red;
-  Gizmos.DrawWireSphere(transform.position, lookRadius);
- }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
 }
